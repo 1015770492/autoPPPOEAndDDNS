c7ed0c078f48fbecb9ff696abc44938a0fb5da2a
angel <1015770492@qq.com>
2021-02-21 01:53:00 +0800

    第一次提交

--- src/main/java/top/yumbo/config/InternetConfig.java
+++ src/main/java/top/yumbo/config/InternetConfig.java
@@ -5,13 +5,13 @@ package top.yumbo.config;
  * 全局配置
  */
 public class InternetConfig {
-    public static String pppoeName = "13879307576";         // 宽带连接名称
-    public static String pppoeAccount = "13879307576@lan";  // 宽带账号
-    public static String pppoePassword = "343235";          // 宽带密码
-    public static String accessKeyId = "LTAI4G5RUgc3HYKjpxaxhAuL";// 阿里云上获取accessKeyId
-    public static String accessKeySecret = "aH0ZmXMmpXp1GmGPzfvguVde3ibvL4";// accessKeySecret
-    public static String domainName = "huashengshu.top";// 自己的域名
-    public static String recordType = "AAAA";// 记录类型，A，CNAME，AAAA等
-    public static String RR="@";// 主机记录：例如取 @ 或 www 或 * 等
+    public static String pppoeName = "";      // 宽带连接名称
+    public static String pppoeAccount = "";   // 宽带账号
+    public static String pppoePassword = "";  // 宽带密码
+    public static String accessKeyId = "";    // 阿里云上获取accessKeyId
+    public static String accessKeySecret = "";// accessKeySecret
+    public static String domainName = "";     // 自己的域名
+    public static String recordType = "AAAA"; // 记录类型，A，CNAME，AAAA等
+    public static String RR="@";              // 主机记录：例如取 @ 或 www 或 * 等
 
 }
